41526cf2f82f5cc37ab604444c794648234cfec4
Enrique Romo <enrique.romo0611@gmail.com>
2022-10-06 18:45:53 -0500

    Tarea 3 finalizada

---
+++ Tareas/Tarea 3/Tarea_3.py
@@ -0,0 +1,11 @@
+# Empezar bucle
+while True:
+    # Obtener valor
+    valor = input("Introduce algo: ")
+    # Condicion: si el valor es igual a "salir", termina el bucle
+    if valor == "salir":
+        break
+    else:
+        print(valor)
+# Fin del bucle
+print("fin del codigo")
