145e2a6938e2bbbb822a9ae66e71e2cafe64bf76
Enrique Romo <enrique.romo0611@gmail.com>
2022-11-03 21:30:05 -0500

    Tarea 2, Function, finalizada

---
+++ Tareas/Tarea 2/Tarea_2_Function.py
@@ -0,0 +1,11 @@
+
+#funcion
+def numero(num1):
+    #iclo for
+  for num in range(1, 11):
+    print(num1, "x", num, "=", num1*num)
+
+num1 = int(input("Escribe un numero: "))
+numero(num1)
+
+# Ciclo for
