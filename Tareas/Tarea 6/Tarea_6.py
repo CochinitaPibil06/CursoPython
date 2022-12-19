8fc43da0d631335e6ec339a878e2e86a51cb4ba8
Enrique Romo <enrique.romo0611@gmail.com>
2022-12-19 14:28:19 -0500

    Tarea 6 finalizada

---
+++ Tareas/Tarea 6/Tarea_6.py
@@ -0,0 +1,9 @@
+DiasSemana = ("Lunes", "martes", "miercoles", "jueves", "viernes", "Sabado", "Domingo")
+
+print("Dias de la semana:", DiasSemana)
+
+(*EntreSemana, Sabado, Domingo) = DiasSemana
+FinSemana = (Sabado, Domingo)
+
+print("Dias entre semana:", tuple(EntreSemana))
+print("Fin de semana:" , FinSemana)
